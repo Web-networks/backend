0ec85ce813389e6e98f57da7c8e08aa15a6552f4
Nikita Sikalov <sikalov@yandex-team.ru>
2020-06-10 22:53:16 +0300

    add neuro models description

---
+++ src/models/layerModel.ts
@@ -0,0 +1,41 @@
+import mongoose, { Document, Types, Model } from 'mongoose';
+import { INeuroModel } from 'models/neuroModel';
+
+const LayerShema = new mongoose.Schema({
+    model: {
+        type: Types.ObjectId,
+        ref: 'NeuroModel',
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    params: {
+        type: Object,
+        required: true,
+        default: {},
+    },
+});
+
+interface ILayerShema extends Document {
+    type: string;
+    params: Record<string, any>;
+}
+
+interface ILayerBase extends ILayerShema {
+    // pass
+}
+
+export interface ILayer extends ILayerBase {
+    model: INeuroModel['_id'];
+}
+
+export interface ILayerPopulated extends ILayer {
+    model: INeuroModel;
+}
+
+export interface ILayerModel extends Model<ILayer> {
+    // pass
+}
+
+export const layerModel = mongoose.model<ILayer>('Layer', LayerShema);
